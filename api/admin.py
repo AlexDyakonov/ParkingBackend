66d98328aba4266ba29662701c5464393a2b02ab
AlexDyakonov <shram.monolit@mail.ru>
2023-11-25 12:33:35 +0300

    Added admin and added verbose name in models

--- api/admin.py
+++ api/admin.py
@@ -1,13 +1,40 @@
 from django.contrib import admin
 from api.models import Parking, Coordinate, Category, Price, Space, Location, LocationCoordinate, PriceParking
 
-# Register your models here.
+@admin.register(Coordinate)
+class CoordinateAdmin(admin.ModelAdmin):
+    list_display = ('longitude', 'latitude')
 
-admin.site.register(Parking)
-admin.site.register(Coordinate)
-admin.site.register(Category)
-admin.site.register(Price)
-admin.site.register(Space)
-admin.site.register(Location)
-admin.site.register(LocationCoordinate)
-admin.site.register(PriceParking)
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('location_type',)
+    list_filter = ('location_type',)
+
+@admin.register(LocationCoordinate)
+class LocationCoordinateAdmin(admin.ModelAdmin):
+    list_display = ('location', 'coordinate')
+    list_filter = ('location', 'coordinate')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('zone_purpose',)
+    list_filter = ('zone_purpose',)
+
+@admin.register(Space)
+class SpaceAdmin(admin.ModelAdmin):
+    list_display = ('handicapped', 'total')
+
+@admin.register(Price)
+class PriceAdmin(admin.ModelAdmin):
+    list_display = ('vehicle_type', 'min_price', 'max_price')
+    list_filter = ('vehicle_type',)
+
+@admin.register(Parking)
+class ParkingAdmin(admin.ModelAdmin):
+    list_display = ('blocked', 'aggregating', 'category', 'location', 'center', 'space')
+    list_filter = ('blocked', 'aggregating', 'category', 'location')
+
+@admin.register(PriceParking)
+class PriceParkingAdmin(admin.ModelAdmin):
+    list_display = ('price', 'parking')
+    list_filter = ('price', 'parking')
--- api/models.py
+++ api/models.py
@@ -5,6 +5,9 @@ class Coordinate(models.Model):
     longitude = models.DecimalField(max_digits=10, decimal_places=7)
     latitude = models.DecimalField(max_digits=10, decimal_places=7)
 
+    class Meta:
+        verbose_name_plural = "Координаты"
+
 
 class Location(models.Model):
     class LocationType(models.TextChoices):
@@ -17,10 +20,18 @@ class Location(models.Model):
         choices=LocationType.choices
     )
 
+    class Meta:
+        verbose_name_plural = "Локации"
+    
+
 
 class LocationCoordinate(models.Model):
     location = models.ForeignKey(to=Location, null=False, on_delete=models.CASCADE)
     coordinate = models.ForeignKey(to=Coordinate, null=False, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "Локация-Координаты"
+        # db_table_comment = "Сопоставляет id локации с id координат. Необходимо для задания нескольких координат к одной локации."
 
 
 class Category(models.Model):
@@ -33,10 +44,15 @@ class Category(models.Model):
         choices=CategoryType.choices
     )
 
+    class Meta:
+        verbose_name_plural = "Категория"
+
 
 class Space(models.Model):
     handicapped = models.IntegerField(null=False)
     total = models.IntegerField(null=False)
+    class Meta:
+        verbose_name_plural = "Парковочные места"
 
 
 class Price(models.Model):
@@ -50,6 +66,9 @@ class Price(models.Model):
     min_price = models.IntegerField(null=False)
     max_price = models.IntegerField(null=False)
 
+    class Meta:
+        verbose_name_plural = "Цены"
+
 
 class Parking(models.Model):
     blocked = models.BooleanField(null=False, default=False)
@@ -58,8 +77,14 @@ class Parking(models.Model):
     location = models.ForeignKey(to=Location, null=False, on_delete=models.PROTECT)
     center = models.ForeignKey(to=Coordinate, null=False, on_delete=models.PROTECT)
     space = models.ForeignKey(to=Space, null=False, on_delete=models.PROTECT)
+    class Meta:
+        verbose_name_plural = "Парковки"
 
 
 class PriceParking(models.Model):
     price = models.ForeignKey(to=Price, null=False, on_delete=models.CASCADE)
     parking = models.ForeignKey(to=Parking, null=False, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "Цены парковок"
+        # db_table_comment = "Сопоставляет парковку с возможными на ней ценами."
